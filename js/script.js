819afbdc44c6849f54716ee504d38752b9dc352a
Chirag Mangukiya <cdmi.chirag@gmail.com>
2023-12-19 00:42:22 +0530

    customize

--- js/script.js
+++ js/script.js
@@ -1,40 +1,3 @@
-//-------------------------- Start CountDown Timer----------------------------
-
-// Set the date we're counting down to
-var countDownDate = new Date("Jan 5, 2024 15:37:25").getTime();
-
-// Update the count down every 1 second
-var x = setInterval(function() {
-
-  // Get today's date and time
-  var now = new Date().getTime();
-
-  // Find the distance between now and the count down date
-  var distance = countDownDate - now;
-
-  // Time calculations for days, hours, minutes and seconds
-  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-  // Display the result in the element with id="demo"
-  document.getElementById("days").innerHTML = days + " <span class='time_label'>Days</span>"
-  document.getElementById("hour").innerHTML =  hours + " <span class='time_label'>Hours</span>"
-  document.getElementById("minute").innerHTML = minutes + " <span class='time_label'>Minutes</span>"
-  document.getElementById("second").innerHTML = seconds + " <span class='time_label'>Seconds</span>"
-
-  // If the count down is finished, write some text
-  if (distance < 0) {
-    clearInterval(x);
-    document.getElementById("demo").innerHTML = "EXPIRED";
-  }
-}, 1000);
-
-//-------------------------- End CountDown Timer----------------------------
-
-//-------------------------- Start Client Logo----------------------------
-
 $(document).ready(function(){
 
   $('#client_logo').owlCarousel({
@@ -90,12 +53,8 @@ $(document).ready(function(){
   })
 
 
-
-})
-
 //-------------------------- End Client Logo----------------------------
 
-$(document).ready(function(){
 
   var counted = 0;
   $(window).scroll(function() {
@@ -130,4 +89,45 @@ $(document).ready(function(){
 
   });
 
+
+
 })
+
+//-------------------------- Start CountDown Timer----------------------------
+
+// Set the date we're counting down to
+var countDownDate = new Date("Jan 5, 2024 15:37:25").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+  // Get today's date and time
+  var now = new Date().getTime();
+
+  // Find the distance between now and the count down date
+  var distance = countDownDate - now;
+
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Display the result in the element with id="demo"
+  // document.getElementById("days").innerHTML = days + " <span class='time_label'>Days</span>";
+  // document.getElementById("hour").innerHTML =  hours + " <span class='time_label'>Hours</span>";
+  // document.getElementById("minute").innerHTML = minutes + " <span class='time_label'>Minutes</span>";
+  // document.getElementById("second").innerHTML = seconds + " <span class='time_label'>Seconds</span>";
+
+  // If the count down is finished, write some text
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("demo").innerHTML = "EXPIRED";
+  }
+}, 1000);
+
+//-------------------------- End CountDown Timer----------------------------
+
+//-------------------------- Start Client Logo----------------------------
+
+
